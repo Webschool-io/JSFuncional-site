93209c4152618667474102fe6acbc903b3514bbe
Rodrigo Teobaldo <rgomes090@gmail.com>
2015-10-02 01:46:39 -0300

    Corrigido alguns bugs do responsivo, adicionado meta tags do fb, alterado quebra de linha de alguns arquivos para LF ao inves de CRLF

--- site/src/js/main.js
+++ site/src/js/main.js
@@ -1,2 +1,17 @@
 // put your code here
 // remember, you can use modules with browserify
+(function () {
+    'use strict';
+
+    window.addEventListener('DOMContentLoaded', function () {
+        var menuLinks = document.querySelectorAll('.menuContent a');
+        var menuToggle = document.querySelector('#menuToggle');
+
+        for (var i = 0; i < menuLinks.length; i++) {
+            menuLinks[i].addEventListener('click', function () {
+                menuToggle.checked = false;
+            });
+        }
+
+    });
+})();
